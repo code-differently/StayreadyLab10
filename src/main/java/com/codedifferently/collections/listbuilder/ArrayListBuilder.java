8542be29cc635ce8dfc7eb76697ffa4f9b99566e
Kaveesha <weerasirikaveesha@gmail.com>
2020-07-21 14:16:55 -0500

    finished the arrayListBuilder

--- src/main/java/com/codedifferently/collections/listbuilder/ArrayListBuilder.java
+++ src/main/java/com/codedifferently/collections/listbuilder/ArrayListBuilder.java
@@ -8,21 +8,43 @@ import java.util.List;
 public class ArrayListBuilder implements ListBuilder {
 
     public List buildList(Object[] a) {
-        List<Object> newList = Arrays.asList(a);
-        return newList;
+        List listVersion = Arrays.asList(a);
+        ArrayList<Object> arrayListVersion = new ArrayList<Object>(listVersion);
+        return arrayListVersion;
     }
 
     public List buildList(Collection c) {
-        ArrayList<Collection> newList = new ArrayList<>(c);
-        return newList;
+        ArrayList<Collection> collectionList = new ArrayList<Collection>(c);
+        return collectionList;
     }
 
     public List buildList(Object[] a1, Object[] a2) {
-        return null;
+        Object[] combinedArr = new Object[a1.length + a2.length];
+        List combinedList = copyOverElementsAndTurnIntoList(combinedArr, a1, a2);
+        ArrayList<Object> combinedArrayList = new ArrayList<Object>(combinedList);
+        return combinedArrayList;
+    }
+
+    private List copyOverElementsAndTurnIntoList(Object[] combinedArr, Object[] a1, Object[] a2) {
+        int overallIndex = 0;
+        for(Object a1Element: a1) {
+            combinedArr[overallIndex] = a1Element;
+            overallIndex++;
+        }
+        for(Object a2Element: a2) {
+            combinedArr[overallIndex] = a2Element;
+            overallIndex++;
+        }
+        List combinedList = Arrays.asList(combinedArr);
+        return combinedList;
     }
 
     public List buildList(Collection c1, Collection c2) {
-        return null;
+        List<Collection> nonArrList = buildList(c1);
+        nonArrList.addAll(c2);
+
+        ArrayList <Collection> combinedArrayList = new ArrayList<Collection>(nonArrList);
+        return combinedArrayList;
     }
 
 }
--- src/test/java/com/codedifferently/collections/iterator/ArrayListBuilderTest.java
+++ src/test/java/com/codedifferently/collections/iterator/ArrayListBuilderTest.java
@@ -9,19 +9,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ArrayListBuilderTest {
-    ArrayListBuilder arrList;
+    ArrayListBuilder arrListBuilder;
+    Integer[] firstArray;
+    Integer[] secondArray;
+    ArrayList<Integer> firstCollection;
+    ArrayList<Integer> secondCollection;
 
     @Before
     public void setup() {
-        arrList = new ArrayListBuilder();
+        arrListBuilder = new ArrayListBuilder();
+
+        firstArray = new Integer[1];
+        secondArray = new Integer[1];
+        firstArray[0] = 2;
+        secondArray[0] = 5;
+
+        firstCollection = new ArrayList<Integer>();
+        secondCollection = new ArrayList<Integer>();
+        firstCollection.add(56);
+        secondCollection.add(12);
     }
 
     @Test
     public void buildListWithObjectArrayTest() {
         int expectedNumReturned = 2;
-        Integer[] inputArray = {2};
 
-        List<Integer> newArrList = arrList.buildList(inputArray);
+        List newList = arrListBuilder.buildList(firstArray);
+        ArrayList<Integer> newArrList = new ArrayList<Integer>(newList);
         int actualNumReturned = newArrList.get(0);
 
         Assert.assertEquals(expectedNumReturned, actualNumReturned);
@@ -30,22 +44,33 @@ public class ArrayListBuilderTest {
     @Test
     public void buildListWithCollectionTest() {
         int expectedNum = 56;
-        ArrayList<Integer> collection = new ArrayList<Integer>();
-        collection.add(56);
 
-        List<Integer> newList = arrList.buildList(collection);
-        int actualNum = newList.get(0);
+        List newList = arrListBuilder.buildList(firstCollection);
+        ArrayList<Integer> newArrList =  new ArrayList<Integer>(newList);
+        int actualNum = newArrList.get(0);
 
         Assert.assertEquals(expectedNum, actualNum);
     }
 
     @Test
     public void buildListWithTwoObjectArraysTest() {
+        int expectedSize = 2;
 
+        List newList = arrListBuilder.buildList(firstArray, secondArray);
+        ArrayList<Integer> newArrList =  new ArrayList<Integer>(newList);
+        int actualSize = newArrList.size();
+
+        Assert.assertEquals(expectedSize, actualSize);
     }
 
     @Test
     public void buildListWithTwoCollectionsTest() {
+        int expectedSize = 2;
 
+        List newList = arrListBuilder.buildList(firstCollection, secondCollection);
+        ArrayList<Integer> newArrList = new ArrayList<Integer>(newList);
+        int actualSize = newArrList.size();
+
+        Assert.assertEquals(expectedSize, actualSize);
     }
 }
