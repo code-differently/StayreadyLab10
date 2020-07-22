eed1a4bdc9a6b7fb143322aca7848dbdf5f9075e
madlynjoa1 <maddlynjoa1@icloud.com>
2020-07-22 11:27:14 -0400

    Ready to review

--- src/main/java/com/codedifferently/collections/iterator/FibonacciIterator.java
+++ src/main/java/com/codedifferently/collections/iterator/FibonacciIterator.java
@@ -1,4 +1,26 @@
 package com.codedifferently.collections.iterator;
 
-public class FibonacciIterator {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class FibonacciIterator implements Iterator<Integer> {
+    Collection<Integer> c = new ArrayList();
+    Iterator<Integer> iter = c.iterator();
+    Integer current=0, previous=1;
+
+    @Override
+    public boolean hasNext() {
+        return true;
+    }
+
+    @Override
+    public Integer next(){
+
+           Integer temp = previous;
+            previous += current;
+            current = temp;
+            return temp;
+        }
+
 }
