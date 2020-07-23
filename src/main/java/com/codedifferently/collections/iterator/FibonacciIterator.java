c0bceebf5f526d0154f4c5b4e0cc00786f089c7f
KJ Witherspoon <50391760+Spooncodes@users.noreply.github.com>
2020-07-23 12:37:18 -0400

    updated Fib

--- src/main/java/com/codedifferently/collections/iterator/FibonacciIterator.java
+++ src/main/java/com/codedifferently/collections/iterator/FibonacciIterator.java
@@ -1,4 +1,27 @@
 package com.codedifferently.collections.iterator;
 
-public class FibonacciIterator {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.function.Consumer;
+
+public class FibonacciIterator implements Iterator<Integer> {
+//    Collection<Integer> c = new ArrayList();
+//    Iterator<Integer> iter = c.iterator();
+    Integer current=0, previous=1;
+
+    @Override
+    public boolean hasNext() {
+        return true;
+    }
+
+    @Override
+    public Integer next(){
+
+           Integer temp = previous;
+            previous += current;
+            current = temp;
+            return temp;
+        }
+
 }
