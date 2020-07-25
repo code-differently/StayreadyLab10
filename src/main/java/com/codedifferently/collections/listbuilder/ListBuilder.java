8ae1b55ce581bf23480062dfff85110fceb66218
Faith Wilkins El <44947772+lavendermistyco@users.noreply.github.com>
2020-07-25 12:08:55 -0400

    Finished List Builders

--- src/main/java/com/codedifferently/collections/listbuilder/ListBuilder.java
+++ src/main/java/com/codedifferently/collections/listbuilder/ListBuilder.java
@@ -1,4 +1,17 @@
 package com.codedifferently.collections.listbuilder;
 
+import java.util.Collection;
+import java.util.List;
+
 public interface ListBuilder {
+
+  public List buildList(Object[] a); 
+
+  public List buildList(Collection c); 
+
+  public List buildList(Object[] a, Object[] ab); 
+
+  public List buildList(Collection c, Collection c2); 
+
+
 }
